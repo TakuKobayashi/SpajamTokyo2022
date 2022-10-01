d7b28ecbd839615761b1581fe6d34705f3bc4bbc
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-01 15:55:33 +0900

    leftとrightに変えた

--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -4,7 +4,7 @@ import fastify from 'fastify';
 const app = fastify();
 
 app.get('/poling/vote', async (request, reply) => {
-  return { hello: 'world' };
+  return { left: 1, right: 1 };
 });
 
 export const handler = awsLambdaFastify(app);
