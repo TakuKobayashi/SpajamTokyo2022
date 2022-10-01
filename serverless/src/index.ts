66ae9cf4af14de315b0cef4d58fa0ed91408b9b2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-01 16:49:10 +0900

    firebase にデータを保存したり取り出したりできるようにした

--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -1,7 +1,18 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import fastify from 'fastify';
+import crypto from "crypto";
+import { setupFireStore } from './common/firestore';
+const firestore = setupFireStore();
 
 const app = fastify();
+
+app.get('/', async (request, reply) => {
+  const buff = crypto.randomBytes(8).toString("hex");
+  const currentDoc = firestore.collection("votes").doc(buff);
+  await currentDoc.set({left: 1});
+  const result = await currentDoc.get()
+  return result.data();
+});
 
 app.get('/poling/vote', async (request, reply) => {
   return { left: 1, right: 1 };
