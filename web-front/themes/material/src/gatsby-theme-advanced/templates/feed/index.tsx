78f5d0a5d50c54b5a8282c755b4ba88452dc85ef
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-02 12:22:12 +0900

    ある程度綺麗になったと思う

--- web-front/themes/material/src/gatsby-theme-advanced/templates/feed/index.tsx
+++ web-front/themes/material/src/gatsby-theme-advanced/templates/feed/index.tsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 import { visuallyHidden } from "@mui/utils";
 import YouTube from "react-youtube";
 import { Typography, Button, Box } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 import {
   useConfig,
@@ -71,46 +72,49 @@ const Feed = ({ pageContext }: FeedTemplateProps): JSX.Element => {
   // TODO iframeのYoutube liveの部分は明日
   return (
     <Layout>
-      {getTitleOverride()}
       <ListingPageWrapper ref={feedElementRef}>
-        <YouTube videoId="5EQVoIUQlHw" />
-        <iframe id="inlineMapbox"
+        <Typography variant="h3">台風の予想進路</Typography>
+        <iframe
+          id="inlineMapbox"
           title="Inline Mapbox"
           width="60%"
           height="480"
-          src="https://elix.jp/test/mapbox/typhoon/?caption=no&drag=no&interval=2">
-        </iframe>
-        <Box
-          sx={{
-            width: "100%",
-          }}
-        >
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => onVoteRequest("left")}
-          >
-            左に投票する
-          </Button>
-          <Button
-            variant="contained"
-            color="warning"
-            onClick={() => onVoteRequest("right")}
-          >
-            右に投票する
-          </Button>
+          src="https://elix.jp/test/mapbox/typhoon/?caption=no&drag=no&interval=2"
+        ></iframe>
+        <Box textAlign="center">
+          <Grid container spacing={10}>
+            <Grid item spacing={{ xs: 6 }}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => onVoteRequest("left")}
+              >
+                左に投票する
+              </Button>
+            </Grid>
+            <Grid item spacing={{ xs: 6 }}>
+              <Button
+                variant="contained"
+                color="warning"
+                onClick={() => onVoteRequest("right")}
+              >
+                右に投票する
+              </Button>
+            </Grid>
+          </Grid>
         </Box>
-        <Box
-          sx={{
-            width: "100%",
-            display: "grid",
-            gap: { zero: 8, lg: 10 },
-            gridTemplateColumns: { zero: "1fr", lg: "1fr 1fr" },
-          }}
-        >
-          {voteResult.left}
-          {voteResult.right}
+        <Box textAlign="center">
+          <Grid container spacing={10}>
+            <Grid item spacing={{ xs: 6 }}>
+              <Typography variant="h5">{voteResult.left}</Typography>
+            </Grid>
+            <Grid item spacing={{ xs: 6 }}>
+              <Typography variant="h5">{voteResult.right}</Typography>
+            </Grid>
+          </Grid>
         </Box>
+        <Typography variant="h3">台風の進み状況</Typography>
+        <YouTube videoId="5EQVoIUQlHw" />
       </ListingPageWrapper>
     </Layout>
   );
