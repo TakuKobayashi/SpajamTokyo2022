4e0a0f8bd86c10f2373529bd4bca39c9c9f849b1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-10-02 13:47:03 +0900

    アプリで表示させた

--- Android/app/src/main/java/net/taptappun/taku/kobayashi/spajamtokyo2022/MainActivity.kt
+++ Android/app/src/main/java/net/taptappun/taku/kobayashi/spajamtokyo2022/MainActivity.kt
@@ -1,11 +1,16 @@
 package net.taptappun.taku.kobayashi.spajamtokyo2022
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webview = findViewById<WebView>(R.id.app_webview)
+        webview.getSettings().setJavaScriptEnabled(true)
+        webview.loadUrl("https://wondrous-eclair-9b7b3f.netlify.app/")
     }
 }
